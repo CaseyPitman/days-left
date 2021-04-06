8315368c2cf3295d3a9b772dd47269623596007e
Casey The Great <casey@caseypitman.com>
2021-04-06 10:10:47 -0500

    SHIP

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 //Determine number of days until vacation
 
+//Today's date
 const todayDate = new Date();
-const summerBreak= new Date('June 4, 2021');
+//Scheduled dates
+const summerBreak = new Date('June 4, 2021');
 const schoolOut = new Date('May 27, 2021');
 const beachTrip = new Date('July 8, 2021');
 
-
-const timeConst = 1000 * 60 * 60 * 24;
-
-
+//Calculate time until scheduled Date
 const timeUntilSchoolOut = schoolOut.getTime() - todayDate.getTime();
 const timeUntilSummerBreak = summerBreak.getTime() - todayDate.getTime();
+const timeUntilBeach = beachTrip.getTime() - todayDate.getTime();
 
+//Calculate days until event
+const calculateDays = time => {
+  const timeConst = 1000 * 60 * 60 * 24;
+  return Math.ceil(time / timeConst);
+};
 
-const daysUntilSchoolOut = Math.ceil(timeUntilSchoolOut / timeConst);
-const daysUntilSummerBreak = Math.ceil(timeUntilSummerBreak / timeConst);
-
-
-
-document.getElementById('school-out').innerHTML = daysUntilSchoolOut;
-document.getElementById('summer-break').innerHTML = daysUntilSummerBreak;
-
+//Display days until event
+document.getElementById('school-out').innerHTML = calculateDays(
+  timeUntilSchoolOut
+);
+document.getElementById('summer-break').innerHTML = calculateDays(
+  timeUntilSummerBreak
+);
+document.getElementById('beach-trip').innerHTML = calculateDays(timeUntilBeach);
